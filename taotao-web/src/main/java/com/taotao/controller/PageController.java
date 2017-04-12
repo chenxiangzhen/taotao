ec0cf22289b99845293c3d15c6105934b9f0c59e
chenxiangzhen <chen_xiang_zhen@163.com>
2017-04-12 16:29:45 +0800

    展示后台其他页面

--- taotao-web/src/main/java/com/taotao/controller/PageController.java
+++ taotao-web/src/main/java/com/taotao/controller/PageController.java
@@ -1,6 +1,7 @@
 package com.taotao.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -15,5 +16,10 @@ public class PageController {
     @RequestMapping("/")
     public String showIndex() {
         return "index";
+    }
+
+    @RequestMapping("/{page}")
+    public String showPage(@PathVariable String page) {
+        return page;
     }
 }
