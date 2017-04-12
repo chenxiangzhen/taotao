4b502e5ad0df26937446a29885a05f6cdf7cf3d5
chenxiangzhen <chen_xiang_zhen@163.com>
2017-04-12 15:41:25 +0800

    展示后台首页

---
+++ taotao-web/src/main/java/com/taotao/controller/PageController.java
@@ -0,0 +1,19 @@
+package com.taotao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Description：PageController
+ *
+ * @author: ChenXiangzhen
+ * create 2017-04-12-15:39
+ */
+@Controller
+public class PageController {
+
+    @RequestMapping("/")
+    public String showIndex() {
+        return "index";
+    }
+}
